9ae74ba6c3b2aef68a4be5159872a465a40f082b
dvoroniuc-th <dvoroniuc.themefuse@gmail.com>
2021-03-14 17:28:45 +0200

    generated elixir supervisor project

---
+++ lib/rtp.ex
@@ -0,0 +1,18 @@
+defmodule RTP do
+  @moduledoc """
+  Documentation for `RTP`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RTP.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
